f4809047562dcdb4310b08a03f7039b672e54c51
Hubert Wójtowicz <hubert.wojtowicz@geotik.com>
2020-03-28 15:33:21 +0100

    feat: added role and launch configuration of EC2 instance

--- project2-IaaS/solution/update.sh
+++ project2-IaaS/solution/update.sh
@@ -3,4 +3,5 @@ aws cloudformation update-stack \
 --template-body file://$2 \
 --parameters file://$3 \
 --region=us-west-1 \
---capabilities CAPABILITY_IAM
+--capabilities CAPABILITY_IAM \
+--capabilities CAPABILITY_NAMED_IAM
